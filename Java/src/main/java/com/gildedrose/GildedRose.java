d3f219ad9b7e26a8ab9c820116850f1613a62d2f
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-09-14 10:38:05 -0500

    Test commit - no real changes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,7 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < items.length; i++) { // start loop thru all items
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -57,6 +57,7 @@ class GildedRose {
                     }
                 }
             }
+        // end loop thru all itmes
         }
     }
 }
