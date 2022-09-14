f7a2989b8dff12e1b8bc5794ad1495bd655e7fa1
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-09-14 17:17:24 -0500

    wrote our sulfuras completely, reorganized

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,36 +9,35 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if(items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                if (items[i].quality < 50) {
-                    items[i].quality++;
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && (items[i].sellIn < 11 || items[i].sellIn < 6) && items[i].quality < 50) {
-                        items[i].quality++;
-                    }
-                }
-            } else {
-                // if not aged brie and not concert tickets
-                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // if quality > 0 and not sulfuras
-                    items[i].quality--;
-                }
-            }
-
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // if not sulfuras
                 items[i].sellIn = items[i].sellIn - 1;
-            }
-            if (items[i].sellIn < 0) { // if past sell by date
-                if(items[i].name.equals("Aged Brie")){
-                    if (items[i].quality < 50) { // increase quality if quality < 50
+                if(items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){ // if aged brie or tickets
+                    if (items[i].quality < 50) {
                         items[i].quality++;
+                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && (items[i].sellIn < 11 || items[i].sellIn < 6) && items[i].quality < 50) {
+                            items[i].quality++;
+                        }
                     }
-                } else {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                } else { // if not aged brie and not concert tickets AND not sulfuras
+                    if (items[i].quality > 0) { // if quality > 0
                         items[i].quality--;
+                    }
+                }
+                if (items[i].sellIn < 0) { // if past sell by date
+                    if(items[i].name.equals("Aged Brie")){
+                        if (items[i].quality < 50) { // increase quality if quality < 50
+                            items[i].quality++;
+                        }
                     } else {
-                        items[i].quality = 0; 
+                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality > 0) { // if not concert tickets and quality is > 0
+                            items[i].quality--;
+                        } else { // if concert tickets OR quality <= 0
+                            items[i].quality = 0; 
+                        }
                     }
                 }
             }
+            
         }
     }
 }
