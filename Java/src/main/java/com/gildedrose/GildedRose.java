9740faa38ef26525f0ea9933b7cc33ee41266559
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-09-16 08:48:59 -0500

    refactoring 2

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -27,18 +27,16 @@ class GildedRose {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality--;
-                            }
+            if (items[i].sellIn < 0) { // if past sell in date
+                if (!items[i].name.equals("Aged Brie")) { // if not aged brie
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // if not concert tickets
+                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) { 
+                            items[i].quality--;
                         }
-                    } else {
+                    } else { // if concert tickets
                         items[i].quality = 0;
                     }
-                } else {
+                } else { // if aged brie
                     if (items[i].quality < 50) {
                         items[i].quality++;
                     }
